a982c9a1a3bf8aa36a7e6e9be5e5c29ebc029ece
vicalvez <vicalvez@student.42nice.fr>
2024-06-10 11:08:12 -0400

    fixed wp install

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -6,12 +6,13 @@ while ! mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE &>
 	sleep 3
 done
 
-if [ ! -f "/var/www/html/index.html" ]; then
+if [ ! -f "/var/www/wordpress/index.php" ]; then
 
-	wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-	wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+	cd /var/www/wordpress
+	wp core download --path="/var/www/wordpress" --allow-root
+	wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --dbcharset="utf8" --dbcollate="utf8_general_ci" --dbprefix="wp_" --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
 echo "Started wordpress"
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,10 +1,3 @@
-#!/bin/sh
-
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-
-	echo "wp-config.php file doesnt exists"
-
-	cat << EOF > /var/www/wordpress/wp-config.php
 <?php
 define( 'DB_NAME', getenv('MYSQL_DATABASE') );
 define( 'DB_USER', getenv('MYSQL_USER') );
@@ -20,12 +13,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once ABSPATH . 'wp-settings.php';
-EOF
-
-	echo "wp-config.php created"
-	#rm -f /var/www/wordpress/wp-config-sample.php
-fi
-
-#echo "Wordpress container started"
-#/usr/sbin/php-fpm8 -F
 
